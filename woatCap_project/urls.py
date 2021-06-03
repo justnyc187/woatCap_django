b7362d91cc8c8c2013d2a97259765117e306fdd0
justnyc <jcairo187@gmail.com>
2021-06-03 15:27:02 -0400

    added login

--- woatCap_project/urls.py
+++ woatCap_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls'))
+    path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
