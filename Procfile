b4b79816224d22619daed281dab13b83f3447a29
justnyc <jcairo187@gmail.com>
2021-06-09 16:57:20 -0400

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn {woatCap_project}.wsgi
--- woatCap_project/settings.py
+++ woatCap_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# HEROKU IMPORT
+
+import django_on_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# HEROKU
+django_on_heroku.settings(locals())
