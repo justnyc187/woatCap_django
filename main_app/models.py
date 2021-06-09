7521db1fd239df7a5bde0d4fa7b5bb9026a1bbd7
justnyc <jcairo187@gmail.com>
2021-06-09 12:30:29 -0400

    css

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,27 @@ from datetime import datetime
 
 # Create your models here.
 
-# class Profile(models.Model):
+
+class Sneaker(models.Model):
+
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=10000)
+    size = models.CharField(max_length=15)
+    # value = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Do i need related name to connect sneakers to Profile as well as user?
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at', 'name']
+
+
+        
+
+        # class Profile(models.Model):
 #     name = models.CharField(max_length=500)
 #     hometown = models.CharField(max_length=500)
 #     favoriteShoe = models.CharField(max_length=200)
@@ -18,18 +38,3 @@ from datetime import datetime
 
 #     class Meta:
 #         ordering = ['name']
-
-class Sneaker(models.Model):
-
-    name = models.CharField(max_length=100)
-    image = models.CharField(max_length=10000)
-    size = models.CharField(max_length=15)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # Do i need related name to connect sneakers to Profile as well as user?
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['-created_at', 'name']
